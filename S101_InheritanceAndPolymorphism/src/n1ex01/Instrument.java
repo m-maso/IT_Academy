21023ae12d529a60024296df1a5009b91daca28c
m-maso <146754649+m-maso@users.noreply.github.com>
2023-11-17 18:35:08 +0100

    changes, add code bloc

--- S101_InheritanceAndPolymorphism/src/n1ex01/Instrument.java
+++ S101_InheritanceAndPolymorphism/src/n1ex01/Instrument.java
@@ -24,6 +24,17 @@ public abstract class Instrument {
 		this.preu = preu;
 	}
 	
+	// Bloc inicialització, el crida a cada cop que usem constructor
+	{
+		System.out.println("Bloc inicialització (classe Instr.): Instrument creat");
+	}
+	// Bloc static, el crida el primer cop (abans que el bloc d'inicialització)
+	static {
+		System.out.println("Inicialització bloc static a la classe Instrument");
+	}
 	
+	public static void metodeStatic(){
+		System.out.println("Mètode static a la classe Instrument");
+	}
 	public abstract String tocar();
 }
--- S101_InheritanceAndPolymorphism/src/n1ex01/Principal.java
+++ S101_InheritanceAndPolymorphism/src/n1ex01/Principal.java
@@ -2,22 +2,28 @@ package n1ex01;
 
 public class Principal {
 	
+	//s'inicialitza quan es carrega la classe
 	static 
 	{
-		System.out.println("Comença inicialització bloc static");
+		System.out.println("Inicialització bloc static a la classe Principal\n");
 	}
-
+	
+	
 	public static void main(String[] args) {
 		
-		Instrument corda1 = new InstrCorda("violí",1000);
-		System.out.println(corda1.tocar());
-	
-		Instrument vent1 = new InstrVent("clarinet", 600);
-		System.out.println(vent1.tocar());
+		System.out.println("Càrrega provocada per la creació de la primera instància");
 		
-		Instrument perc1 = new InstrPercussio("xilòfon", 200);
-		System.out.println(perc1.tocar());
+		InstrCorda corda1 = new InstrCorda("violí",1000);
+		System.out.println(corda1.tocar() + "\n");
+	
+		InstrVent vent1 = new InstrVent("clarinet", 600);
+		System.out.println(vent1.tocar() + "\n");
+		
+		InstrPercussio perc1 = new InstrPercussio("xilòfon", 200);
+		System.out.println(perc1.tocar() + "\n");
 
+		System.out.println("Càrrega provocada per l'accés a un membre static"); 
+		Instrument.metodeStatic();
 	}
 
 }
