ba5cace9563636ddeefcc51642e124de8140bc0c
m-maso <146754649+m-maso@users.noreply.github.com>
2023-11-17 18:36:44 +0100

    changes static attribute

--- S101_InheritanceAndPolymorphism/src/n1ex02/Cotxe.java
+++ S101_InheritanceAndPolymorphism/src/n1ex02/Cotxe.java
@@ -3,18 +3,22 @@ package n1ex02;
 public class Cotxe {
 	
 	//attrib
-	private static final String marca = "Honda";
-	private static String model;
-	private final int POTENCIA = 1600;
+		//static final(const. de classe): no pot inicialitzar en el constr.,no modif., mateix valor x tots obj.
+	private static final String MARCA = "Honda"; 
+		//static: pot inicialitzar en el constr., si modif. modif tots obj., mateix valor x tots obj. 
+	private static String model;	
+		//final(constant): pot inicialitzar en el constr., no modif.
+	private final int POTENCIA;					 
 	
 	//constr
-	public Cotxe(String model) {
-		Cotxe.model = model;
+	public Cotxe() {
+		Cotxe.model = "Jazz";
+		this.POTENCIA = 1600;
 	}
 	
 	//getters
-	public static String getMarca() {
-		return marca;
+	public static String getMARCA() {
+		return MARCA;
 	}
 	public static String getModel() {
 		return model;
@@ -22,6 +26,7 @@ public class Cotxe {
 	public int getPOTENCIA() {
 		return this.POTENCIA;
 	}
+	
 	//setters
 	public static void setModel(String model) {
 		Cotxe.model = model;
@@ -35,5 +40,11 @@ public class Cotxe {
 	
 	public void accelerar() {
 		System.out.println("El vehicle està accelerant");
+	}
+	
+	//own methods
+	@Override
+	public String toString() {
+		return "Cotxe-> marca: " + Cotxe.MARCA + ", model: " + Cotxe.model + ", potencia: " + this.POTENCIA;
 	}
 }
--- S101_InheritanceAndPolymorphism/src/n1ex02/Principal.java
+++ S101_InheritanceAndPolymorphism/src/n1ex02/Principal.java
@@ -3,12 +3,20 @@ package n1ex02;
 public class Principal {
 
 	public static void main(String[] args) {
-		// invocar mètode estàtic i no estàtic
 		
+		System.out.println("Invocar mètode static (classe):");
 		Cotxe.frenar();
 		
-		Cotxe cotxe1 = new Cotxe("Civic");
+		System.out.println("\nInvocar mètode no static(instància de la classe):");
+		Cotxe cotxe1 = new Cotxe();
 		cotxe1.accelerar();
+		
+		System.out.println("\ncotxe1: " + cotxe1.toString());
+		
+		//establim l'únic atribut que no és una constant
+		Cotxe cotxe2 = new Cotxe();
+		Cotxe.setModel("Civic");
+		System.out.println("cotxe2 (canviem el model[atrib. static]): " + cotxe2.toString());
 	}
 
 }
