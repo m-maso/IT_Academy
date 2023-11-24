fdc61ad2585f4cd70f4b62fe63d66ae0c0f670e7
m-maso <146754649+m-maso@users.noreply.github.com>
2023-11-24 06:36:04 +0100

    simplify filter

--- S108_Lambdas/src/n1ex01/Main.java
+++ S108_Lambdas/src/n1ex01/Main.java
@@ -3,7 +3,6 @@ package n1ex01;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -11,19 +10,10 @@ public class Main {
 
 		List<String> strList = new ArrayList<String>();
 		strList.addAll(Arrays.asList("Hello","How", "are","you","add","all","strings","Old"));
-		
-//		strList.forEach(word->System.out.println(word));
 
-		/* Printing words directly */
-		strList.stream().filter(word->word.contains("o") || word.contains("O"))
+		/* Printing words directly, without a previous saved list */
+		strList.stream().filter(word->word.toLowerCase().contains("o"))
 						.forEach(word->System.out.println(word));
-		
-		/* Printing words from a previous saved list */
-		
-		List<String> wordsWithO = new ArrayList<String>();
-		wordsWithO = strList.stream()
-						.filter(word->word.contains("o") || word.contains("O"))
-						.collect(Collectors.toList());
-		System.out.println("Printing words from a previous saved list" + wordsWithO);
+
 	}
 }
