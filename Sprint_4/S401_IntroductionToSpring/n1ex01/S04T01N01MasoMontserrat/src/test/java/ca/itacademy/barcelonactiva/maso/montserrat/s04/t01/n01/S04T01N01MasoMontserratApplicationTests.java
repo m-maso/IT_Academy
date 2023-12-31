870a42a72bb16d72e62a6f3ad523d08e6150b0ba
m-maso <146754649+m-maso@users.noreply.github.com>
2023-12-31 16:09:31 +0100

    add n1ex01 Sprint 4

---
+++ Sprint_4/S401_IntroductionToSpring/n1ex01/S04T01N01MasoMontserrat/src/test/java/ca/itacademy/barcelonactiva/maso/montserrat/s04/t01/n01/S04T01N01MasoMontserratApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.itacademy.barcelonactiva.maso.montserrat.s04.t01.n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01MasoMontserratApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
