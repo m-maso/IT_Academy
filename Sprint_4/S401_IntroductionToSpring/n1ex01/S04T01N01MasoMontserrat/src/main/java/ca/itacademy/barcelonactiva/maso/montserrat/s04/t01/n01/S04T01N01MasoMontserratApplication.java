25060dc1f0d8f811f3270f1deb3245b8a4b572fb
m-maso <146754649+m-maso@users.noreply.github.com>
2023-12-31 14:26:41 +0100

    add n1ex01 in Sprint 4

---
+++ Sprint_4/S401_IntroductionToSpring/n1ex01/S04T01N01MasoMontserrat/src/main/java/ca/itacademy/barcelonactiva/maso/montserrat/s04/t01/n01/S04T01N01MasoMontserratApplication.java
@@ -0,0 +1,13 @@
+package ca.itacademy.barcelonactiva.maso.montserrat.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01MasoMontserratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01MasoMontserratApplication.class, args);
+	}
+
+}
